4fdedc3070dffb52cfdba92d1a1c953e9725a931
chutchens08 <chrishutchens5@gmail.com>
2018-01-08 04:58:27 -0600

    added three lines multi-cursor lines to init

--- init.el
+++ init.el
@@ -156,3 +156,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-c a") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
