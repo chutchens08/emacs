f60efa1cf274c2b4df654041698ace5ae2956675
chutchens08 <chrishutchens5@gmail.com>
2018-01-20 17:24:10 -0600

    added redditor mode

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m org-drill)))
  '(package-selected-packages
    (quote
-    (minimap ace-window which-key hydra fireplace org-plus-contrib multiple-cursors magit ace-jump-mode google-this bongo dired-sidebar pianobar auctex switch-window windresize define-word org)))
+    (request hierarchy minimap ace-window which-key hydra fireplace org-plus-contrib multiple-cursors magit ace-jump-mode google-this bongo dired-sidebar pianobar auctex switch-window windresize define-word org)))
  '(pdf-latex-command "xetex")
  '(scroll-bar-mode nil)
  '(send-mail-function (quote smtpmail-send-it))
@@ -175,6 +175,12 @@
 
 ;; ------------------- Minimap Mode --------------------
 (global-set-key (kbd "C-c m") 'minimap-mode)
+
+;; ------------------- Redditor Mode -------------------
+;; Both Heirarchy and Request added to get this to work.
+;; So don't freak out because you don't know what they are doing up top.
+(add-to-list 'load-path "~/.emacs.d/vendor/redditor-mode")
+(require 'redditor-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
